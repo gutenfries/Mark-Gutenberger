c78fb698e9fb8626cce3cc3db8d6aedc4d552a49
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2021-10-12 12:50:41 -0500

    delete unused scripts

--- public/src/assets/js/redirect.js
+++ public/src/assets/js/redirect.js
@@ -1,9 +1,16 @@
 /*
-pretty starightforward stuff, call this in an ext file with
+pretty starightforward stuff, call this in an externl file with
 the redirect(x) function, making `x` arg the desired new location.
-newlocation is allowef to be a hyperlink or a rel ('/' or '/home')
+newlocation is allowed to be a hyperlink or a relatve link ('/' or '/home').
 make sure you url (aka percent) encode anything and put it in single quotes ''.
 */
 function redirect(newlocation) {
 	window.location = newlocation;
 }
+/*
+Example callback (html):
+<script href="<...link to this file...>/redirect.js" type="text/javascript">
+redirect('./'); //goes up one directory
+redirect('https://save418.com'); //goes to hyper link
+redirect('pages/about-me/hello.html') //goes to hello.html in pages
+*/
