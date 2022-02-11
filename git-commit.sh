2e98ca52d5c2842aef24aa761d28e23d39d88697
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-02-11 14:15:14 +0000

    debug git logic [#10]

--- git-commit.sh
+++ git-commit.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 function git-commitf() {
-      git config user.name $1
-      git config user.email $2
+      git config user.name "$1"
+      git config user.email "$2"
       git add -A .
       # git commit -m "DevOps: Generate 3D Contrib svgs"
       # git push
       git diff --quiet
-      git diff --staged --quiet || git commit -m $3
+      git diff --staged --quiet || git commit -m "$3"
 }
