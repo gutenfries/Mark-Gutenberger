77fd6e957989f9f7a484af0fc1cacb7947897945
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-02-11 13:58:51 +0000

    attempt to add git logic

---
+++ .github/workflows/git-commit.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+function git-commitf( committer_name, committer_email, commit_message ) {
+      git config user.name ${committer_name}
+      git config user.email ${committer_email}
+      git add -A .
+      # git commit -m "DevOps: Generate 3D Contrib svgs"
+      # git push
+      git diff --quiet 
+      git diff --staged --quiet || git commit -am ${commit_message}
+
+}
