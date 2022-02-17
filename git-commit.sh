e42b03568e36b534449677a681147fd1fee57baf
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-02-17 13:41:34 -0600

    replace spaces in bash indentation with tabs

--- git-commit.sh
+++ git-commit.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 function git-commitf() {
-      git config user.name "$1"
-      git config user.email "$2"
-      git add -A .
-      git diff --quiet
-      git diff --staged --quiet || git commit -m "$3" && git push
+		git config user.name "$1"
+		git config user.email "$2"
+		git add -A .
+		git diff --quiet
+		git diff --staged --quiet || git commit -m "$3" && git push
 }
